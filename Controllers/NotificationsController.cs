f0a45de0f7cd9efb02f0d57df395b489b08db0b4
Evan <ehutch7@gmail.com>
2021-04-07 19:58:40 -0400

    Testing out a POST endpoint on the NotificationsController.

--- Controllers/NotificationsController.cs
+++ Controllers/NotificationsController.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using TravelAgentWeb.Data;
+using TravelAgentWeb.Dtos;
+using TravelAgentWeb.Models;
 
 namespace TravelAgentWeb.Controllers
 {
@@ -12,6 +16,29 @@ namespace TravelAgentWeb.Controllers
         public NotificationsController(TravelAgentDbContext context)
         {
             this._context = context;
+        }
+
+        [HttpPost]
+        public ActionResult FlightChanged(FlightDetailUpdateDto flightDetailUpdateDto)
+        {
+            Console.WriteLine($"Webhook Received! {flightDetailUpdateDto}");
+
+            WebhookSecret secretModel = this._context.SubscriptionSecrets.FirstOrDefault(s => s.Publisher == flightDetailUpdateDto.Publisher && s.Secret == flightDetailUpdateDto.Secret);
+
+            if (secretModel == null)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid Secret -- Ignore Webhook");
+                Console.ResetColor();
+                return Ok();
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Valid Webhook!");
+            Console.WriteLine($"Old Price {flightDetailUpdateDto.OldPrice}, New Price {flightDetailUpdateDto.NewPrice}");
+            Console.ResetColor();
+            return Ok();
+
         }
     }
 }
